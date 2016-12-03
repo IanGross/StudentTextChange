b1a43ebb7e892817bef4498b9409624635249a31
Ian Gross <grossi2@rpi.edu>
2016-12-03 16:10:40 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
     <script type="text/javascript" src="resources/jquery-1.4.3.min.js"></script>
     <script type="text/javascript" src="resources/iit.js"></script>   
-    <link href="resources/iit.css" rel="stylesheet" type="text/css"/>
-	<link href="resources/mysite.css" rel="stylesheet" type="text/css"/>
+    <link href="resources/ii.css" rel="stylesheet" type="text/css"/>
+	<link href="http://localhost/iitforms/resources/mysite.css" rel="stylesheet" type="text/css"/>
   </head>
   <body>
 	<div class="header">
