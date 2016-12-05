fe96074b6bd437dfc0a6096edb8d7aa514f7985e
Ian Gross <grossi2@rpi.edu>
2016-12-05 12:14:31 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'localhost');
+   define('DB_DATABASE', 'iit');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
