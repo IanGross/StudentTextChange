132824488574e5cfa10a11e246711167c453deaa
Ian Gross <grossi2@rpi.edu>
2016-12-06 00:04:01 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,78 +4,124 @@
     <title>Profile</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link href="resources/mysite.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  
+	<?php
+	include("config.php");
+	session_start();
+	$user_get = $_SESSION["login_user"];
+	$searchTerm = '';
+	$tmp = '';
+	?>
+	
+	
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="inventory.php">Student TextChange</a>
+	    </div>
+	    <ul class="nav navbar-nav navbar-left">
+	      <li><a href="newItem.php">Add New Item</a></li>
+	      <li><a href="profile.php">Profile</a></li>
+	    </ul>
+		<ul class="nav navbar-nav navbar-right">
+	      <li><a href="profile.php">Logged in as <?php echo $user_get;?></a></li>
+	    </ul>
+		<!--
+		<div class = "search_cent">
+		  <form name="searchBox" target="" method="post">
+			<input type="text" class="search" value="<?php echo $searchTerm;?>" name="searchIt" id="searchIt" placeholder="Search..">
+		  </form>
+		</div>
+		-->
+	  </div>
+	</nav>
+	
+  </head>
+  
+  
   </head>
 
   <body>
 	<!--<div id="bodyBlock">-->
-    <div class="upperleft">
-      <form action="inventory.php">
-        <button type="submit">Back</button>
-      </form>
-    </div>
+	<?php 
+  
+	// have we posted?
+	$havePost = isset($_POST["deleteBtn"]);
+  
+	if ($havePost) {
+		echo "hello";
+		$tmp = $_POST["lblItemName"];
+		echo $tmp;
+		$sql = "DELETE FROM inventory WHERE item_id = '$tmp'";
+
+		if (mysqli_query($db,$sql) === TRUE) { $message = "Record has been deleted succesfully";
+			echo "<script type='text/javascript'>alert('$message');</script>"; } 
+		else { echo "Error: " . $sql . "<br>" . $db->error; }
+		
+	}
+	?>
+	
+	
 
     <div class="header">
       <h1>Profile</h1>
     </div>
 
-    <div class="left">
+    <div class="profile_item">
 
       <div class="header">
         <h2>Change Password</h2>
       </div>
 
       <form action="profile.php">
+          <div class="subitem"><h3><label class="field">Current Email:</label></h3>
+          <h4><?php echo $user_get;?></h4></div>
 
+		  <div class="subitem"><h3><label class="field">Enter Old Password:</label></h3>
+		  <input type="text" size="25" value=""/></div>
 
+          <div class="subitem"><h3><label class="field">Enter New Password:</label></h3>
+          <input type="text" size="25" value=""/></div>
+		  </br>
 
-          <h3><label class="field">Email:</label></h3>
-          <div class="Email"><h4>Place Holder</h4></div>
-
-          <h3><label class="field">Enter Old Password:</label></h3>
-          <div class="value"><input type="text" size="25" value=""/></div>
-
-          <h3><label class="field">Enter New Password:</label></h3>
-          <div class="value"><input type="text" size="25" value=""/></div>
-
-          <button type="submit">Save</button>
-
-
-
+          <input type="submit" value="Submit" id="save" name="save"/><br/>
       </form>
     </div>
 
 
 
-    <div class="right">
+    <div class="profile_item">
 
       <div class="header">
         <h2>Delete Items</h2>
       </div>
 	  
 	  <?php
-	  include("config.php");
-	  session_start();
-	  
-	  $user_get = $_SESSION["login_user"];
-	  $sql = "SELECT item_name, date_added FROM inventory WHERE inventory.username = '$user_get' ";
+	  $sql = "SELECT item_id, item_name, date_added FROM inventory WHERE inventory.username = '$user_get' ";
       $result = mysqli_query($db,$sql);
 	  
 	  
 	  if ($result->num_rows > 0) {
 			 // output data of each row
 			 while($row = $result->fetch_assoc()) {
-				 ?>
-				 <div class="right_item">
-				 
-				 <?php
-				 //echo '<div class=\"userItem_single\">';
+
+				  
+				  
+				 echo"<div class=\"user_items\">";
 				 echo"<h3><u>Item Name</u>: ".$row["item_name"]."</h3>";
 				 echo"<h3><u>Time Created</u>: ".$row["date_added"]."</h3>";
-				 //echo '</div><br>';
+				 
 				 ?>
-				 </div>
+				 
+				 <form name="delete-item" method="POST" action="profile.php">
+					 <input type="submit" value="DELETE" name="deleteBtn" style="color:black;float:right;"/>
+					 <input type="hidden" name="lblItemName" value="<?php echo $row["item_id"]; ?>">
+				 </form>
 				 
 				 <?php
+				 echo"</br></div>";
+				 
 			}
 		} else {
 			 echo "You have 0 items created, go add a new item :D";
@@ -83,14 +129,6 @@
 	  
 	  
 	  ?>
-		<!--
-        <form action="inventory.html">
-
-          <h3><label class="field">Text Book Title</label></h3>
-          <button type="submit">Delete</button>
-
-        </form>
-		-->
 
     </div>
 	<!--</div>-->
