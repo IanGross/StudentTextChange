4b82630919487a504d7187c349df5c77eed38033
Ian Gross <grossi2@rpi.edu>
2016-12-06 22:42:28 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,9 +5,23 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
     <script type="text/javascript" src="resources/jquery-1.4.3.min.js"></script>
     <script type="text/javascript" src="resources/iit.js"></script>   
-    <link href="resources/ii.css" rel="stylesheet" type="text/css"/>
-	<!--<link href="http://localhost/iitforms/resources/mysite.css" rel="stylesheet" type="text/css"/>-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link href="resources/mysite.css" rel="stylesheet" type="text/css"/>
+	
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">Student TextChange</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li class="active"><a href="welcome.html">Home</a></li>
+	      <li><a href="login.php">Log in</a></li>
+	      <li><a href="signup.php">Sign Up</a></li>
+	    </ul>
+	  </div>
+	</nav>
+	<div class="container">
+	
   </head>
   <body>
 	<div class="header">
@@ -28,21 +42,16 @@
   $havePost = isset($_POST["save"]);
   
   if ($havePost) {
+	  
+   include("config.php");
+   session_start();
     // Get the input and clean it.
     // First, let's get the input one param at a time.
     // Could also output escape with htmlentities()
 	
     $loginUserName = htmlspecialchars(trim($_POST["loginUserName"]));  
     $loginUserPassword = htmlspecialchars(trim($_POST["loginUserPassword"]));
-	  $loginUserPasswordVal = htmlspecialchars(trim($_POST["loginUserPasswordVal"]));
-    
-    // special handling for the date of birth
-    //$dobTime = strtotime($dob); // parse the date of birth into a Unix timestamp (seconds since Jan 1, 1970)
-    //$dateFormat = 'Y-m-d'; // the date format we expect, yyyy-mm-dd
-    // Now convert the $dobTime into a date using the specfied format.
-    // Does the outcome match the input the user supplied?  
-    // The right side will evaluate true or false, and this will be assigned to $dobOk
-    //$dobOk = (date($dateFormat, $dobTime) == $dob);  
+	$loginUserPasswordVal = htmlspecialchars(trim($_POST["loginUserPasswordVal"])); 
     
     // Let's do some basic validation
     $focusId = ''; // trap the first field that needs updating, better would be to save errors in an array
@@ -79,60 +88,41 @@
     <?php } else { ?>
 	  
 	  <?php
-		$servername = "localhost";
-		$username = "root";
-		$password = "localhost";
-		$dbname = "iit";
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		} 
-		$sql = "INSERT INTO users (username, password)
-		VALUES ('$loginUserName', '$loginUserPassword');";
-		if ($conn->multi_query($sql) === TRUE) { $message = "New records created successfully";
+		$sql = "INSERT INTO users (username, password) VALUES ('$loginUserName', '$loginUserPassword');";
+		if (mysqli_query($db,$sql) === TRUE) { $message = "User Credentials stored successfully";
 			echo "<script type='text/javascript'>alert('$message');</script>"; } 
-		else { echo "Error: " . $sql . "<br>" . $conn->error; }
-		$conn->close();
+		else { echo "Error: " . $sql . "<br>" . $db->error; }
 		
 		$loginUserName = '';  
 		$loginUserPassword = '';
 		$loginUserPasswordVal = '';
 		
 		?>
-	  
-	  
     <?php } 
   }
 ?>
 
-<?php 
-  // to include client-side validation to the form below, 
-  // add the following parameter:
-  // onsubmit="return validate(this);"
-?>
 
-
-<div class="header">
+	<div class="header">
       <h2>Sign Up</h2>
     </div>
 
-    <form id="loginForm" name="loginForm" method="post" action="login.php">
+    <form id="loginForm" name="loginForm" method="post" action="signup.php">
 		<fieldset> 
 
-		  <div class="middle">
-			<h3><label class="field">Email (Use your RPI Email Address):</label></h3>
-			<div class="value"><input type="text" size="25" value="<?php echo $loginUserName; ?>" name="loginUserName" id="loginUserName"/></div>
+		  <div class="middle_items">
+			<h3><label class="form_desc" style="text-align:right;">Email (Use your RPI Email Address):</label></h3>
+			<div class="value"><input type="text" size="25" value="<?php echo $loginUserName; ?>" style="color:black;float:center;" name="loginUserName" id="loginUserName"/></div>
 
-			<h3><label class="field">Password:</label></h3>
-			<div class="value"><input type="password" size="25" value="<?php echo $loginUserPassword; ?>" name="loginUserPassword" id="loginUserPassword"/></div>
+			<h3><label class="form_desc" style="text-align:right;">Password:</label></h3>
+			<div class="value"><input type="password" size="25" value="<?php echo $loginUserPassword; ?>" style="color:black;float:center;" name="loginUserPassword" id="loginUserPassword"/></div>
 
-			<h3><label class="field">Confirm Password:</label></h3>
-			<div class="value"><input type="password" size="25" value="<?php echo $loginUserPasswordVal; ?>" name="loginUserPasswordVal" id="loginUserPasswordVal"/></div>
+			<h3><label class="form_desc" style="text-align:right;">Confirm Password:</label></h3>
+			<div class="value"><input type="password" size="25" value="<?php echo $loginUserPasswordVal; ?>" style="color:black;float:center;" name="loginUserPasswordVal" id="loginUserPasswordVal"/></div>
 
 			
-			<input type="submit" value="save" id="save" name="save"/>
+			<input type="submit" value="Save" id="save" name="save" style="color:black;float:right;"/>
+			</br>
 		
 		  </div>
 	   </fieldset> 
