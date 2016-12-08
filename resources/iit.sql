e3a322d79229a1dd5734e63a3dae0e7a2a9b7004
Ian Gross <grossi2@rpi.edu>
2016-12-07 23:10:10 -0500

    Update iit.sql

--- resources/iit.sql
+++ resources/iit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2016 at 06:41 AM
+-- Generation Time: Dec 08, 2016 at 05:08 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.24
 
@@ -44,10 +44,12 @@ CREATE TABLE `inventory` (
 --
 
 INSERT INTO `inventory` (`item_id`, `item_name`, `item_condition`, `description`, `price`, `category`, `contact_info`, `full_name`, `username`, `date_added`) VALUES
-(9, 'Plastic Plates', 'Good', 'Only had for a year. Sturdy and have served me well', 13, 'Other', 'email: grossi2@rpi.edu', 'Ian Gross', 'grossi2', '2016-12-04 19:25:14'),
 (11, 'Space Jam', 'Brand New', 'Come on and slam, and welcome to the jam\r\nCome on and slam, if you wanna jam', 30, 'Other', 'gb me', 'Igy F', 'grossi2', '2016-12-05 12:30:39'),
 (14, 'TI-84+', 'Very Good', 'Fresh from the thrift shop, down the road', 239.45, 'Electronics', 'instatweet-a-tumbler-gram me at iggyG', 'Meaty', 'grossi2', '2016-12-06 00:10:52'),
-(18, 'Information Systems: A Manager''s Guide to Harnessing Technology, v. 1.4', 'Very Good', 'Used in Managing IT Resources for Fall 2016. Hard Cover Version', 65.45, 'Textbook', 'Phone Number: XXX-XXX-XXXX', 'Ian Gross', 'grossi2', '2016-12-06 00:34:12');
+(18, 'Information Systems: A Manager''s Guide to Harnessing Technology, v. 1.4', 'Very Good', 'Used in Managing IT Resources for Fall 2016. Hard Cover Version', 65.45, 'Textbook', 'Phone Number: XXX-XXX-XXXX', 'Ian Gross', 'grossi2', '2016-12-06 00:34:12'),
+(19, 'Fundamentals of Physics, 9th Edition', 'Like New', 'Barely Used, bought a semester ago', 75, 'Textbook', 'Tweet at me @rpi_yamato', 'Meatwad', 'senior', '2016-12-07 15:59:06'),
+(20, 'Principles of Modern Operating Systems, 2nd ed.', 'Acceptable', 'Used in Operating Systems. Passed down from two other people so it has a bit of wear and tear.', 25, 'Textbook', 'Message me on Facebook. Username: Mad Dog', 'Luchadoor', 'senior', '2016-12-07 20:02:20'),
+(21, 'Nachos', 'Brand New', 'I just bought them and I do not want them anymore', 2.4, 'Other', 'I am in Mcneil right now', 'Ian', 'seniorsenior', '2016-12-07 23:03:16');
 
 -- --------------------------------------------------------
 
@@ -65,8 +67,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `password`) VALUES
+('GeorgeLopez@rpi.edu', 'taco'),
 ('grossi2', 'student'),
+('laptophi', 'hello'),
+('robotguy', '123'),
 ('senior', 'taco'),
+('seniorsenior', 'senior'),
+('ss', 'ss'),
 ('user', 'pass');
 
 --
@@ -94,7 +101,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `item_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `item_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
