080d543b6a86db5a637ed49d6e59ace8e8d01657
Ian Gross <grossi2@rpi.edu>
2016-12-07 23:06:13 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Forms with PHP - ITWS</title> 
+    <title>Signup</title> 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
     <script type="text/javascript" src="resources/jquery-1.4.3.min.js"></script>
     <script type="text/javascript" src="resources/iit.js"></script>   
@@ -20,41 +20,28 @@
 	    </ul>
 	  </div>
 	</nav>
-	<div class="container">
-	
   </head>
   <body>
 	<div class="header">
       <h1>Student Text-Change</h1>
     </div>
 	
-<?php 
-  /* some very basic form processing */
-  
-  // variables to hold our form values:
+  <?php 
   $loginUserName = '';  
   $loginUserPassword = '';
   $loginUserPasswordVal = '';
-  // hold any error messages
   $errors = ''; 
   
-  // have we posted?
   $havePost = isset($_POST["save"]);
   
   if ($havePost) {
-	  
-   include("config.php");
-   session_start();
-    // Get the input and clean it.
-    // First, let's get the input one param at a time.
-    // Could also output escape with htmlentities()
+    include("config.php");
+    session_start();
 	
     $loginUserName = htmlspecialchars(trim($_POST["loginUserName"]));  
     $loginUserPassword = htmlspecialchars(trim($_POST["loginUserPassword"]));
 	$loginUserPasswordVal = htmlspecialchars(trim($_POST["loginUserPasswordVal"])); 
-    
-    // Let's do some basic validation
-    $focusId = ''; // trap the first field that needs updating, better would be to save errors in an array
+    $focusId = '';
     
     if ($loginUserName == '') {
       $errors .= '<li>First name may not be blank</li>';
@@ -86,7 +73,6 @@
         </script>
       </div>
     <?php } else { ?>
-	  
 	  <?php
 		$sql = "INSERT INTO users (username, password) VALUES ('$loginUserName', '$loginUserPassword');";
 		if (mysqli_query($db,$sql) === TRUE) { $message = "User Credentials stored successfully";
@@ -96,13 +82,10 @@
 		$loginUserName = '';  
 		$loginUserPassword = '';
 		$loginUserPasswordVal = '';
-		
 		?>
     <?php } 
   }
-?>
-
-
+  ?>
 	<div class="header">
       <h2>Sign Up</h2>
     </div>
@@ -120,10 +103,8 @@
 			<h3><label class="form_desc" style="text-align:right;">Confirm Password:</label></h3>
 			<div class="value"><input type="password" size="25" value="<?php echo $loginUserPasswordVal; ?>" style="color:black;float:center;" name="loginUserPasswordVal" id="loginUserPasswordVal"/></div>
 
-			
 			<input type="submit" value="Save" id="save" name="save" style="color:black;float:right;"/>
-			</br>
-		
+			<br>
 		  </div>
 	   </fieldset> 
     </form>
