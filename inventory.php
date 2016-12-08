3176ce344265d06cfaa5c88bc8599421bd08f4a6
Ian Gross <grossi2@rpi.edu>
2016-12-07 22:59:22 -0500

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -1,131 +1,100 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>Forms with PHP - ITWS</title> 
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
-    <script type="text/javascript" src="resources/jquery-1.4.3.min.js"></script>
-    <script type="text/javascript" src="resources/iit.js"></script>   
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link href="resources/mysite.css" rel="stylesheet" type="text/css"/>
+    <title>Inventory</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link href="resources/mysite.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	
-	<nav class="navbar navbar-inverse">
+	
+	<?php
+	include("config.php");
+	session_start();
+	$user_get = $_SESSION["login_user"];
+	$searchTerm = '';
+	?>
+	
+	
+	<nav class="navbar navbar-inverse navbar-fixed-top">
 	  <div class="container-fluid">
 	    <div class="navbar-header">
-	      <a class="navbar-brand" href="#">Student TextChange</a>
+	      <a class="navbar-brand" href="inventory.php">Student TextChange</a>
 	    </div>
-	    <ul class="nav navbar-nav">
-	      <li class="active"><a href="welcome.html">Home</a></li>
-	      <li><a href="login.php">Log in</a></li>
-	      <li><a href="signup.php">Sign Up</a></li>
+	    <ul class="nav navbar-nav navbar-left">
+	      <li><a href="newItem.php">Add New Item</a></li>
+	      <li><a href="profile.php">Profile</a></li>
 	    </ul>
+		<ul class="nav navbar-nav navbar-right">
+	      <li><a href="profile.php">Logged in as <?php echo $user_get;?></a></li>
+		  <li><a class="btn btn-danger" href="welcome.html" onclick="alert('You have successfully Logged out')"><font color="white">Logout</font></a></li>
+	    </ul>
+		<div class = "search_cent">
+		  <form name="searchBox" target="" method="post">
+			<input type="text" class="search" value="<?php echo $searchTerm;?>" name="searchIt" id="searchIt" placeholder="Search..">
+		  </form>
+		</div>
 	  </div>
 	</nav>
-	<div class="container">
+	<br><br><br>
 	
   </head>
   <body>
-	<div class="header">
-      <h1>Student Text-Change</h1>
+    <div class="header">
+      <h1><b>Inventory</b></h1>
     </div>
-	
-<?php 
-  /* some very basic form processing */
+	<br>
+    <fieldset>
+      <div class="middle">
+		<?php
+		$money_symbol="$";
+		$sql = "SELECT item_name, item_condition, description, price, category, contact_info FROM inventory ORDER BY date_added DESC";
+		$havePost = isset($_POST["searchIt"]);
   
-  // variables to hold our form values:
-  $loginUserName = '';  
-  $loginUserPassword = '';
-  $loginUserPasswordVal = '';
-  // hold any error messages
-  $errors = ''; 
-  
-  // have we posted?
-  $havePost = isset($_POST["save"]);
-  
-  if ($havePost) {
-	  
-   include("config.php");
-   session_start();
-    // Get the input and clean it.
-    // First, let's get the input one param at a time.
-    // Could also output escape with htmlentities()
-	
-    $loginUserName = htmlspecialchars(trim($_POST["loginUserName"]));  
-    $loginUserPassword = htmlspecialchars(trim($_POST["loginUserPassword"]));
-	$loginUserPasswordVal = htmlspecialchars(trim($_POST["loginUserPasswordVal"])); 
-    
-    // Let's do some basic validation
-    $focusId = ''; // trap the first field that needs updating, better would be to save errors in an array
-    
-    if ($loginUserName == '') {
-      $errors .= '<li>First name may not be blank</li>';
-      if ($focusId == '') $focusId = '#loginUserName';
-    }
-    if ($loginUserPassword == '') {
-      $errors .= '<li>Last name may not be blank</li>';
-      if ($focusId == '') $focusId = '#loginUserPassword';
-    }
-    if ($loginUserPasswordVal == '') {
-      $errors .= '<li>Matching Password may not be blank</li>';
-      if ($focusId == '') $focusId = '#loginUserPasswordVal';
-    }
-	if ($loginUserPassword != $loginUserPasswordVal) {
-      $errors .= '<li>Passwords must be matching</li>';
-      if ($focusId == '') $focusId = '#loginUserPasswordVal';
-    }
-  
-    if ($errors != '') { ?>
-      <div id="messages">
-        <h4>Please correct the following errors:</h4>
-        <ul>
-          <?php echo $errors; ?>
-        </ul>
-        <script type="text/javascript">
-          $(document).ready(function() {
-            $("<?php echo $focusId ?>").focus();
-          });
-        </script>
+		if ($havePost) {
+			$searchTerm = htmlspecialchars(trim($_POST["searchIt"]));
+			unset($sql);
+			$searchTermBits = array("item_name LIKE '%$searchTerm%'", "description LIKE '%$searchTerm%'", "item_condition LIKE '%$searchTerm%'");
+			$sql = "SELECT item_name, item_condition, description, price, category, contact_info FROM inventory
+			WHERE " .implode(' OR ', $searchTermBits). " ORDER BY date_added DESC";	
+		}
+		
+		$result = mysqli_query($db,$sql);
+		
+		if ($havePost) {echo "Search Term: \"$searchTerm\" yielded $result->num_rows result(s)";}
+		
+
+		if ($result->num_rows > 0) {
+			 // output data of each row
+			 while($row = $result->fetch_assoc()) {
+				 ?> <div class = inventory_item> <?php
+				 echo"<br><div class=\"title\"><h2>".$row["item_name"]."</h2></div>";
+				 
+				 echo"<div class=\"subitem\"><h3><label><u>Category</u>: &nbsp;&nbsp;&nbsp;&nbsp;</label><font color=\"black\"> ";
+				 echo $row["category"]."</font></h3></div>";
+				 
+				 echo"<div class=\"subitem\">";
+				 echo"<h3><label><u>Asking Price</u>: &nbsp;&nbsp;&nbsp;&nbsp;</label><font color=\"black\"> ";
+				 echo"$money_symbol". $row["price"]."</font></h3>";
+				 echo"</div>";
+				 
+				 echo"<div class=\"subitem\"><h3><label><u>Condition</u>: &nbsp;&nbsp;&nbsp;&nbsp;</label><font color=\"black\">";
+				 echo $row["item_condition"]."</font></h3></div>";
+				 
+				 echo"<div class=\"subitem\"><h3><label><u>Description</u>: &nbsp;&nbsp;&nbsp;&nbsp;</label><font color=\"black\">";
+				 echo $row["description"]."</font></h3></div>";
+				 
+				 echo"<div class=\"subitem\"><h3><label><u>Contact Info</u>: &nbsp;&nbsp;&nbsp;&nbsp;</label><font color=\"black\">";
+				 echo $row["contact_info"]."</font></h3></div>";
+
+				 ?> </div><?php
+				 echo "</br>";
+			}
+		} else {
+			 echo "0 results";
+		}
+		?>  
       </div>
-    <?php } else { ?>
-	  
-	  <?php
-		$sql = "INSERT INTO users (username, password) VALUES ('$loginUserName', '$loginUserPassword');";
-		if (mysqli_query($db,$sql) === TRUE) { $message = "User Credentials stored successfully";
-			echo "<script type='text/javascript'>alert('$message');</script>"; } 
-		else { echo "Error: " . $sql . "<br>" . $db->error; }
-		
-		$loginUserName = '';  
-		$loginUserPassword = '';
-		$loginUserPasswordVal = '';
-		
-		?>
-    <?php } 
-  }
-?>
-
-
-	<div class="header">
-      <h2>Sign Up</h2>
-    </div>
-
-    <form id="loginForm" name="loginForm" method="post" action="signup.php">
-		<fieldset> 
-
-		  <div class="middle_items">
-			<h3><label class="form_desc" style="text-align:right;">Email (Use your RPI Email Address):</label></h3>
-			<div class="value"><input type="text" size="25" value="<?php echo $loginUserName; ?>" style="color:black;float:center;" name="loginUserName" id="loginUserName"/></div>
-
-			<h3><label class="form_desc" style="text-align:right;">Password:</label></h3>
-			<div class="value"><input type="password" size="25" value="<?php echo $loginUserPassword; ?>" style="color:black;float:center;" name="loginUserPassword" id="loginUserPassword"/></div>
-
-			<h3><label class="form_desc" style="text-align:right;">Confirm Password:</label></h3>
-			<div class="value"><input type="password" size="25" value="<?php echo $loginUserPasswordVal; ?>" style="color:black;float:center;" name="loginUserPasswordVal" id="loginUserPasswordVal"/></div>
-
-			
-			<input type="submit" value="Save" id="save" name="save" style="color:black;float:right;"/>
-			</br>
-		
-		  </div>
-	   </fieldset> 
-    </form>
+    </fieldset>
   </body>
 </html>
+
